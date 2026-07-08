139e839cfe59fc41ee2a5f4d8f011558c38f18a5
guimendespy <guilhermemendes9531@gmail.com>
2026-07-08 00:37:14 -0300

    versao 1.0 subtrator

---
+++ rtl/subtrator.sv
@@ -0,0 +1,11 @@
+import vending_pkg::*;
+
+module subtrator (
+  input  logic [7:0] credit,
+  input  logic [7:0] price,
+  output logic [7:0] change 
+);
+
+  assign change = credit - price;
+
+endmodule
